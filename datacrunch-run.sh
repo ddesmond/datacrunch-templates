83247b78befe4af96a1fece00fe8b405ab4d4fd3
Aleks <crni.aki@gmail.com>
2025-08-05 12:01:05 +0200

    init

--- datacrunch-run.sh
+++ datacrunch-run.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+echo "Setting up Comfy!"
+
+cd /opt
+
+git pull https://github.com/ddesmond/datacrunch-templates.git
+cd datacrunch-templates
+
+# rundown
+bash init.sh
+bash deps.sh
+cd comfy
+bash comfy-setup.sh
+bash comfy-nodes.sh
