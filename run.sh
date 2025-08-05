5d68d424ade94c20515c5dd1f8db4fe1086b304e
Aleks <crni.aki@gmail.com>
2025-08-05 15:07:23 +0200

    init

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "_____ RUN COMFYUI _____"
 export PIP_ROOT_USER_ACTION=ignore
-#source /opt/comfy-extra/env.sh
+source /datacrunch-templates/env.sh
 python3 main.py --listen 0.0.0.0 --port 8188 --output-directory /outputs/ --disable-xformers --preview-method auto
