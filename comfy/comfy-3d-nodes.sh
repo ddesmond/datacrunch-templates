3d18cc9b3588d66db0ee46ff25651870404cb528
Aleks <crni.aki@gmail.com>
2025-08-06 07:29:46 +0200

    init

--- comfy/comfy-3d-nodes.sh
+++ comfy/comfy-3d-nodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Run: Setup comfy nodes!"
+echo "Run: Setup 3D comfy nodes!"
 
 for repo in \
 https://github.com/ssitu/ComfyUI_UltimateSDUpscale.git \
@@ -14,25 +14,16 @@ https://github.com/ltdrdata/ComfyUI-Impact-Pack.git \
 https://github.com/Fannovel16/comfyui_controlnet_aux.git \
 https://github.com/yolain/ComfyUI-Easy-Use.git \
 https://github.com/kijai/ComfyUI-Florence2.git \
-https://github.com/ShmuelRonen/ComfyUI-LatentSyncWrapper.git \
 https://github.com/WASasquatch/was-node-suite-comfyui.git \
 https://github.com/theUpsider/ComfyUI-Logic.git \
 https://github.com/cubiq/ComfyUI_essentials.git \
-https://github.com/chrisgoringe/cg-image-picker.git \
-https://github.com/chflame163/ComfyUI_LayerStyle.git \
-https://github.com/chrisgoringe/cg-use-everywhere.git \
 https://github.com/ClownsharkBatwing/RES4LYF \
-https://github.com/welltop-cn/ComfyUI-TeaCache.git \
-https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git \
-https://github.com/Jonseed/ComfyUI-Detail-Daemon.git \
-https://github.com/kijai/ComfyUI-WanVideoWrapper.git \
-https://github.com/chflame163/ComfyUI_LayerStyle_Advance.git \
 https://github.com/BadCafeCode/masquerade-nodes-comfyui.git \
 https://github.com/1038lab/ComfyUI-RMBG.git \
 https://github.com/crystian/ComfyUI-Crystools \
 https://github.com/ltdrdata/ComfyUI-Manager \
-https://github.com/liusida/ComfyUI-Login.git \
-https://github.com/M1kep/ComfyLiterals.git; \
+https://github.com/kijai/ComfyUI-Hunyuan3DWrapper.git \
+https://github.com/liusida/ComfyUI-Login.git; \
 do \
     cd /opt/ComfyUI/custom_nodes; \
     repo_dir=$(basename "$repo" .git); \
