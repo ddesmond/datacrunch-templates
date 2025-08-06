ae5ac939e142848795f913020aa6f632a6760181
Aleks <crni.aki@gmail.com>
2025-08-06 10:33:57 +0200

    init

--- models/flux.sh
+++ models/flux.sh
@@ -4,6 +4,7 @@ source ./structure.sh
 export MODELS=/opt/models
 echo "Download models list"
 
-echo "Krea"
+echo "FLUX.1"
 wget -P $MODELS/diffusion_models https://huggingface.co/Comfy-Org/FLUX.1-Krea-dev_ComfyUI/resolve/main/split_files/diffusion_models/flux1-krea-dev_fp8_scaled.safetensors
+wget -P $MODELS/diffusion_models https://huggingface.co/black-forest-labs/FLUX.1-Kontext-dev/resolve/main/flux1-kontext-dev.safetensors
 
