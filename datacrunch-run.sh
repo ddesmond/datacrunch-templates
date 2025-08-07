4867d23e4a160f0339dda3dae203099e7cac269a
Aleks <crni.aki@gmail.com>
2025-08-07 06:18:58 +0200

    updates -uv

--- datacrunch-run.sh
+++ datacrunch-run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+echo "----------------------------------"
 echo "Setup work ENVS"
 # SYSTEM
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 export PIP_ROOT_USER_ACTION=ignore
 
-
+echo "----------------------------------"
 echo "Setting up Comfy!"
 cd /opt
 echo "Run: Clone datacrunch comfy setup"
@@ -14,41 +14,48 @@ git clone https://github.com/ddesmond/datacrunch-templates.git
 cd /opt/datacrunch-templates
 git pull
 
-
+echo "----------------------------------"
 # rundown
 echo "Run: Init"
 source init.sh #> /opt/logs-init.txt
 
+echo "----------------------------------"
 echo "Run: Setup Pyenv 3.12"
 source setup_pyenv.sh #> /opt/logs-init.txt
 
 source ~/.bashrc
 
+echo "----------------------------------"
 echo "Run: Eval pyenv and set global"
 eval "$(pyenv init -)"
 pyenv global 3.12
 
-
+echo "----------------------------------"
 echo "Run: Update pip"
 pip install --upgrade pip
 
+echo "----------------------------------"
 echo "Run: UV"
 curl -LsSf https://astral.sh/uv/install.sh | sh     # or source setup_uv.sh
 
+echo "----------------------------------"
 echo "Run: Deps"
 source deps.sh #> /opt/logs-deps.txt
 
-
+echo "----------------------------------"
 echo "Run: Setup Comfy"
 cd /opt/datacrunch-templates/comfy
 source comfy-setup.sh #> /opt/logs-setup.txt
 
+echo "----------------------------------"
 echo "Run: Setup Comfy nodes"
 cd /opt/datacrunch-templates/comfy
 source comfy-nodes.sh #> /opt/logs-nodes.txt
 
-
+echo "----------------------------------"
 echo "Setup done!"
+echo "----------------------------------"
+
 
 echo "Run Comfy app!"
 cd /opt/datacrunch-templates
