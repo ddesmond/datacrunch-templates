1c43063f83b0b2848196aa79cf1f4bd6de37b0e1
Aleks <crni.aki@gmail.com>
2025-08-07 05:00:13 +0200

    updates

--- env.sh
+++ env.sh
@@ -1,2 +1,15 @@
 # add envs here
-echo "Loading env vars"
+echo "LOAD env vars"
+
+export PIP_ROOT_USER_ACTION=ignore
+
+# Setup envs here
+export HF_TOKEN=""
+export HF_ENDPOINT="https://huggingface.co"
+export HF_HUB_ENABLE_HF_TRANSFER=true
+export HF_HUB_ENABLE_HF_TRANSFER_NO_AUTH=true
+export HF_HUB_DOWNLOAD_TIMEOUT=60
+export COMFY_INSTANCE=developer
+
+# export TCNN_CUDA_ARCHITECTURES=86
+# export TORCH_CUDA_ARCH_LIST=8.6
--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-echo "_____ RUN COMFYUI _____"
-export PIP_ROOT_USER_ACTION=ignore
+echo "_____ WARMUP COMFYUI _____"
+
+
+# link back models here
+if [[ -L /opt/ComfyUI/models ]]
+then
+   echo "MODELS is a symlink to a directory. Pass."
+else
+  echo "MODELS is a not symlink to a directory"
+  rm -rf models/
+  ln -sf /opt/models /opt/ComfyUI
+  ls -la .
+  echo "Models relinked. Happy Comfying!"
+fi
+
 source /opt/datacrunch-templates/env.sh
+
+echo "_____ Startup COMFYUI _____"
 cd /opt/ComfyUI
 
 python3 main.py --listen 0.0.0.0 --port 8188 --output-directory /outputs/ --disable-xformers --preview-method auto
