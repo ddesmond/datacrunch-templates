2dac588388b39bbd419abda1d80b2492bbcb59c0
Aleks <crni.aki@gmail.com>
2025-08-07 06:20:27 +0200

    updates -uv

--- setup_uv.sh
+++ setup_uv.sh
@@ -3,6 +3,6 @@
 echo "Run: Setup UV"
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-echo "Run: Setup .bashrc"
+echo "Run: Setup UV .bashrc PATH"
 echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 
