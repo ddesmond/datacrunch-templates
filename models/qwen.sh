628e57d1bdd69c9a952524920a36eadcbae926ed
Aleks <crni.aki@gmail.com>
2025-08-07 05:04:57 +0200

    updates

--- models/qwen.sh
+++ models/qwen.sh
@@ -4,5 +4,7 @@ source ./structure.sh
 export MODELS=/opt/models
 echo "Download models list"
 
-wget -P $MODELS/vae https://huggingface.co/Qwen/Qwen-Image/resolve/main/vae/diffusion_pytorch_model.safetensors
-mv $MODELS/vae/diffusion_pytorch_model.safetensors $MODELS/vae/qwen-image-vae.safetensors
+wget -P $MODELS/diffusion_models https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/diffusion_models/qwen_image_bf16.safetensors
+wget -P $MODELS/text_encoders https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/text_encoders/qwen_2.5_vl_7b.safetensors
+wget -P $MODELS/vae https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors
+
