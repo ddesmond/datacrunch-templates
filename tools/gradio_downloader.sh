a6a008635445cf3c63e49c534d4466521c3a58ed
Aleks <crni.aki@gmail.com>
2025-08-08 08:34:52 +0200

    updates

--- tools/gradio_downloader.sh
+++ tools/gradio_downloader.sh
@@ -2,11 +2,11 @@
 # add gradio app for background model downloads
 
 cd /opt && git clone https://github.com/ddesmond/gradio-downloader.git
-cd gradio-downloader && uv pip install -r requirements.txt
+cd gradio-downloader && uv pip install -r requirements.txt --system
 cd app
 
 # reparse model downloads
 python parse_models_downloads.py /opt/datacrunch-templates/models
 
 # start gradio app
-gradio gradio_downloader.py &
+python gradio_downloader.py &
