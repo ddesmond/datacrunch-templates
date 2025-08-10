a3cb621e3cf6edc256f9770d707afc5909c968f1
Aleks <crni.aki@gmail.com>
2025-08-10 16:25:53 +0200

    updates

--- deps.sh
+++ deps.sh
@@ -10,3 +10,4 @@ apt-get install -y --no-install-recommends \
 uv pip install packaging setuptools wheel --system
 uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128 --system
 uv pip install opencv-python pyyaml gdown triton comfy-cli PyOpenGL-accelerate sageattention --system
+uv pip install hf_transfer --system
