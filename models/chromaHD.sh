bfa72bcc5a3539c00f772947aaf3a749014ffbdb
Aleks <crni.aki@gmail.com>
2025-08-11 18:08:40 +0200

    updates

--- models/chromaHD.sh
+++ models/chromaHD.sh
@@ -7,4 +7,5 @@ echo "ChromHD"
 
 aria2c -d $MODELS/checkpoints https://huggingface.co/lodestones/Chroma1-HD/resolve/main/Chroma-HD.safetensors -o Chroma-HD.safetensors
 
-aria2c -d  $MODELS/vae https://huggingface.co/lodestones/Chroma1-HD/resolve/main/vae/diffusion_pytorch_model.safetensors-o Chroma-HD_vae.safetensors
+aria2c -d  $MODELS/vae https://huggingface.co/lodestones/Chroma/resolve/main/ae.safetensors -o chroma-flux-ae.safetensors
+aria2c -d  $MODELS/text_encoders https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors -o t5xxl_fp16.safetensors
