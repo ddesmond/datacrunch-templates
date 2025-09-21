e40fc93c324e4909ba78676ff26a7058f1e56f82
Aleks <crni.aki@gmail.com>
2025-09-21 06:02:30 +0200

    updates

--- models/flux.sh
+++ models/flux.sh
@@ -7,4 +7,6 @@ echo "Download models list"
 echo "FLUX.1"
 aria2c -d $MODELS/diffusion_models https://huggingface.co/Comfy-Org/FLUX.1-Krea-dev_ComfyUI/resolve/main/split_files/diffusion_models/flux1-krea-dev_fp8_scaled.safetensors -o flux1-krea-dev_fp8_scaled.safetensors
 aria2c -d $MODELS/diffusion_models https://huggingface.co/black-forest-labs/FLUX.1-Kontext-dev/resolve/main/flux1-kontext-dev.safetensors -o flux1-kontext-dev.safetensors
+aria2c -d $MODELS/diffusion_models https://huggingface.co/black-forest-labs/FLUX.1-Fill-dev/resolve/main/flux1-fill-dev.safetensors -o flux1-fill-dev.safetensors
+aria2c -d $MODELS/vae https://huggingface.co/StableDiffusionVN/Flux/resolve/main/Vae/flux_vae.safetensors -o flux_vae.safetensors
 
