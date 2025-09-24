6107b1a4bf5cbdad05cb037fc4b1ff4338253990
Aleks <crni.aki@gmail.com>
2025-09-24 20:03:43 +0200

    updates

--- models/wan21.sh
+++ models/wan21.sh
@@ -11,5 +11,5 @@ aria2c -d $MODELS/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.1_Comf
 aria2c -d $MODELS/diffusion_models https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_t2v_14B_bf16.safetensors -o wan2.1_t2v_14B_bf16.safetensors
 
 aria2c -d $MODELS/loras https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan21_CausVid_14B_T2V_lora_rank32.safetensors -o Wan21_CausVid_14B_T2V_lora_rank32.safetensors
-
+aria2c -d $MODELS/https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors -o wan_2.1_vae.safetensors
 aria2c -d $MODELS/clip_vision https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors -o clip_vision_h.safetensors
