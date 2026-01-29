59a3b910cb35949a349cedc2ad81557632e358a3
Aleks <crni.aki@gmail.com>
2026-01-29 12:46:18 +0100

    updates

---
+++ models/h3d.sh
@@ -0,0 +1,5 @@
+# cuda 12.4
+# wget https://huggingface.co/tencent/Hunyuan3D-2/resolve/main/hunyuan3d-dit-v2-0/model.fp16.safetensors?download=true
+# comfyui-hunyuan3dwrapper/hy3dgen/texgen/custom_rasterizer# python setup.py install
+# wget https://huggingface.co/Comfy-Org/hunyuan3D_2.1_repackaged/resolve/main/hunyuan_3d_v2.1.safetensors
+# checkpoints / diffusion_models
