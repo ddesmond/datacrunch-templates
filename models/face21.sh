badabd8827bcf731523afe779c994fe5c6464596
Aleks <crni.aki@gmail.com>
2026-04-21 19:50:09 +0200

    updates

--- models/face21.sh
+++ models/face21.sh
@@ -4,10 +4,10 @@ source ./structure.sh
 export MODELS=/opt/models
 echo "Download models list"
 
-aria2c -d  $MODELS/diffusion_models https://huggingface.co/paredeyes/Wan2.1_14B_FusionX/resolve/main/wan2.1_Vace_14B_fp16.safetensors -o wan2.1_Vace_14B_fp16.safetensors
+aria2c -d $MODELS/diffusion_models https://huggingface.co/paredeyes/Wan2.1_14B_FusionX/resolve/main/wan2.1_Vace_14B_fp16.safetensors -o wan2.1_Vace_14B_fp16.safetensors
 
 
-aria2c -d  $MODELS/text_encoders https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp16.safetensors -o umt5_xxl_fp16.safetensors
+aria2c -d $MODELS/text_encoders https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp16.safetensors -o umt5_xxl_fp16.safetensors
 
 # loras
 aria2c -d $MODELS/loras https://huggingface.co/Kijai/WanVideo_comfy/resolve/main/Wan21_CausVid_14B_T2V_lora_rank32.safetensors -o Wan21_CausVid_14B_T2V_lora_rank32.safetensors
--- tools/lodgy.sh
+++ tools/lodgy.sh
@@ -4,6 +4,9 @@
 curl https://logdy.dev/install.sh | bash
 
 echo "Lodgy installed successfully."
+echo "--------> lodgy port 8000 for web ui"
+echo "--------> gradio port 8105 for web ui"
+echo "--------> comfy port 8201 for web ui"
 
 # run lodgy
 logdy follow /root/startup.log --ui-ip=0.0.0.0 --ui-pass=xxx &
